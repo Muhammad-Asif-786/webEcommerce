6cf2c832d802ce3408eb99a53f3f848ef970d840
Muhammad-Asif-786 <masifmeyo786@gmail.com>
2026-06-20 16:11:37 +0500

    correct cance button

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,4 +1,4 @@
-export const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5050";
+export const baseURL = import.meta.env.VITE_API_URL || "http://13.49.228.189:5050";
 
 
 const SummaryApi = {
